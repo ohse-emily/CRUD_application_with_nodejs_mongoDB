5df0e30db44d38c2e1d2a11100736b61fa204bf8
ohse-emily <82620945+ohse-emily@users.noreply.github.com>
2021-09-28 22:26:17 +0900

    'update_APIs'

--- server/services/render.js
+++ server/services/render.js
@@ -1,7 +1,16 @@
-
+const axios = require('axios');
 
 exports.homeRoutes = (req,res)=>{
-    res.render('index');
+    
+    // make a get request to /api/users
+    axios.get('http://localhost:3000/api/users')
+        .then(function(response){
+            console.log(`response.data:`,response.data)
+            res.render('index', {users:response.data});
+        })
+        .catch(err=>{
+            res.send(err);
+        })
 }
 
 exports.add_user = (req,res)=>{
@@ -9,5 +18,14 @@ exports.add_user = (req,res)=>{
 }
 
 exports.update_user = (req,res)=>{
-    res.render('update_user');
+    // let id = req.query.id;
+    // axios.get(`http://localhost:3000/api/users?id=${id}`)
+    axios.get('http://localhost:3000/api/users', {params:{id:req.query.id}})
+        .then(function(user_data){
+            console.log(`user_data.data=`, user_data.data)
+            res.render('update_user', {user:user_data.data})
+        })
+        .catch(err=>{
+            res.send(err);
+        })
 } 
